f21910844ff44ba22a1ca34c0368903187f29351
lolusername <atilio.barreda@turbonomic.com>
2019-03-12 16:15:08 -0400

    express init

--- functions/index.js
+++ functions/index.js
@@ -1,38 +1,50 @@
 /* eslint-disable promise/always-return */
 /* eslint-disable prettier/prettier */
 const functions = require("firebase-functions");
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-const axios = require('axios');
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
 
+const apiKey = "3b39d698-63e9-474c-82f8-2de71166af36";
+const url = "https://api.hubapi.com/contacts/v1/contact/?hapikey=" + apiKey;
 
-
-exports.createHsContact = functions.https.onRequest((request, response) => {
-
-    response.send("mood");
+app.post(url, (req,res)=>{
+    response.send("mood-b");
     let data = request.query;
     console.log(data);
-
-    const apiKey = "3b39d698-63e9-474c-82f8-2de71166af36";
-    const url = "https://api.hubapi.com/contacts/v1/contact/?hapikey=" + apiKey;
-
-    let contactObject = {
-        properties: [ 
-            {
-                property: "email",
-                value: data.email
-            },
-            {
-                property: "name",
-                value: data.name
-            }
-        ]};
-
-    axios.post(url, contactObject).then((response) => {
-        console.log(response);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
 });
+
+exports.hubSpotMiddleware = functions.https.onRequest(app);
+
+
+// exports.createHsContact = functions.https.onRequest((request, response) => {
+
+//     response.send("mood");
+//     let data = request.query;
+//     console.log(data);
+
+//     const apiKey = "3b39d698-63e9-474c-82f8-2de71166af36";
+//     const url = "https://api.hubapi.com/contacts/v1/contact/?hapikey=" + apiKey;
+
+//     let contactObject = {
+//         properties: [ 
+//             {
+//                 property: "email",
+//                 value: data.email
+//             },
+//             {
+//                 property: "name",
+//                 value: data.name
+//             }
+//         ]};
+
+//     axios.post(url, contactObject).then((response) => {
+//         console.log(response);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+// });
